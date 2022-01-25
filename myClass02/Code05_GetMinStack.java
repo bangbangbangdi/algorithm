6266ed602af1526b7061ca1c88a3cd01b67cbb9f
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2022-01-25 23:44:43 +0800

    mm

---
+++ myClass02/Code05_GetMinStack.java
@@ -0,0 +1,20 @@
+package myClass02;
+
+
+import java.util.Stack;
+
+public class Code05_GetMinStack {
+    public static class MyStack1 {
+        private Stack<Integer> stackData;
+        private Stack<Integer> stackMin;
+
+        public MyStack1(){
+            this.stackData = new Stack<Integer>();
+            this.stackMin = new Stack<Integer>();
+        }
+
+        public void push(){
+
+        }
+    }
+}
