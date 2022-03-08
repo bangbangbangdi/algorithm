244946a2da8a31c0818cbd2dc6efc77378fe3399
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2022-03-09 00:00:19 +0800

    mm

--- myClass02/Code04_RingArray.java
+++ myClass02/Code04_RingArray.java
@@ -1,6 +1,8 @@
 package myClass02;
 
 
+import java.util.LinkedList;
+
 public class Code04_RingArray {
 
     public static class MyQueue {
@@ -27,8 +29,8 @@ public class Code04_RingArray {
             size++;
         }
 
-        public int pop(int i){
-            if(size<=0){
+        public int pop() {
+            if (size <= 0) {
                 throw new RuntimeException("栈空了");
             }
             int result = arr[polli];
@@ -37,12 +39,65 @@ public class Code04_RingArray {
             return result;
         }
 
-        public boolean isEmpty(){
+        public boolean isEmpty() {
             return size == 0;
         }
 
         public int nextIndex(int index) {
             return index < limit - 1 ? index + 1 : 0;
         }
+    }
+
+    public static class MyStack{
+        public int limit;
+        public int index;
+        public int[] arr;
+        public int size;
+
+        public MyStack(int l){
+            arr = new int[l];
+            limit = l;
+            index = 0;
+            size = 0;
+        }
+
+        public void push(int i){
+            if(size >= limit){
+                throw new RuntimeException("栈满了");
+            }
+        }
+    }
+
+    public static int getRandomInt(int max) {
+        int value = (int) (Math.random() * max - Math.random() * max);
+        return value;
+    }
+
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue(20);
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < 500000; i++) {
+            int size = (int) Math.random() * 20;
+            for (int j = 0; j < size; j++) {
+                int randomInt = getRandomInt(100);
+                if (myQueue.isEmpty() & queue.isEmpty()) {
+                    myQueue.push(randomInt);
+                    queue.push(randomInt);
+                }
+                if (Math.random() < 0.5) {
+                    myQueue.push(randomInt);
+                    queue.push(randomInt);
+                }else{
+                    int myPop = myQueue.pop();
+                    int pop = queue.pop();
+                    if (myPop != pop){
+                        System.out.println("Fuck!");
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.println("Succeed!");
     }
 }
