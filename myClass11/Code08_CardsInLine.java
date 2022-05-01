e90e5eeb6d9e18ba10f5764110675964da173aee
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2022-05-01 10:55:27 +0900

    mm

--- myClass11/Code08_CardsInLine.java
+++ myClass11/Code08_CardsInLine.java
@@ -1,20 +1,36 @@
-//package myClass11;
-//
-//public class Code08_CardsInLine {
-//
-////    给定一个整型数组arr，代表数值不同的纸牌排成一条线，
-////    玩家A和玩家B依次拿走每张纸牌，
-////    规定玩家A先拿，玩家B后拿，
-////    但是每个玩家每次只能拿走最左或最右的纸牌，
-////    玩家A和玩家B都绝顶聪明。请返回最后获胜者的分数。
-//    public static int win1(int[] arr){
-//
-//    }
-//
-//    public static int f(int[] arr){
-//        if (arr.length == 1){
-//            return arr[0];
-//        }
-//    }
-//
-//}
+package myClass11;
+
+public class Code08_CardsInLine {
+
+    //    给定一个整型数组arr，代表数值不同的纸牌排成一条线，
+    //    玩家A和玩家B依次拿走每张纸牌，
+    //    规定玩家A先拿，玩家B后拿，
+    //    但是每个玩家每次只能拿走最左或最右的纸牌，
+    //    玩家A和玩家B都绝顶聪明。请返回最后获胜者的分数。
+    public static int win1(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return 0;
+        }
+        return Math.max(f(arr, 0, arr.length - 1), s(arr, 0, arr.length - 1));
+    }
+
+    public static int f(int[] arr, int i, int j) {
+        if (i == j) {
+            return arr[i];
+        }
+        return Math.max(arr[i] + s(arr, i + 1, j), arr[j] + s(arr, i, j - 1));
+    }
+
+    public static int s(int[] arr, int i, int j) {
+        if (i == j) {
+            return 0;
+        }
+        return Math.min(f(arr, i + 1, j), f(arr, i, j - 1));
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {6, 10, 7, 1};
+        System.out.println(win1(arr));
+    }
+
+}
