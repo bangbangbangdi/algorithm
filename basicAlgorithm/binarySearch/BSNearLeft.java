2685db934d1127816bdf88bbbecf383a782e8a47
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2022-08-16 01:37:44 +0900

    mm

--- basicAlgorithm/binarySearch/BSNearLeft.java
+++ basicAlgorithm/binarySearch/BSNearLeft.java
@@ -4,38 +4,42 @@ import tool.Tools;
 
 import java.util.Arrays;
 
-public class BSExist {
-    // 有序数组下,请找出某数是否存在
-    public static boolean BSExist(int[] arr, int num) {
+public class BSNearLeft {
+
+    // 有序数组下,请找出 >= num 的最左的数
+    public static int BSNearLeft(int[] arr, int num) {
         if (arr == null || arr.length == 0) {
-            return false;
+            return -1;
         }
         int L = 0;
         int R = arr.length - 1;
         int mid = 0;
+        int ans = -1;
         while (L < R) {
             mid = L + ((R - L) >> 1);
-            if (arr[mid] == num) {
-                return true;
-            } else if (arr[mid] < num) {
-                L = mid + 1;
-            } else {
+            if (arr[mid] >= num) {
+                ans = mid;
                 R = mid - 1;
+            } else {
+                L = mid + 1;
             }
         }
-        return arr[L] == num;
+//        ans = arr[L] >= num && L < ans ? L : ans;
+        ans = arr[L] >= num? L : ans;
+        return ans;
     }
 
-    public static boolean comparator(int[] arr, int value) {
+    public static int comparator(int[] arr, int num) {
         if (arr == null || arr.length == 0) {
-            return false;
+            return -1;
         }
-        for (int i : arr) {
-            if (i == value) {
-                return true;
+        int ans = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= num) {
+                return i;
             }
         }
-        return false;
+        return ans;
     }
 
     public static void test() {
@@ -48,10 +52,11 @@ public class BSExist {
             Arrays.sort(arr);
             int randomInt = Tools.getRandomInt(maxValue);
 //            Tools.printArray(arr);
-            if (BSExist(arr, randomInt) != comparator(arr, randomInt)) {
+            if (BSNearLeft(arr, randomInt) != comparator(arr, randomInt)) {
+                succeed = false;
                 Tools.printArray(arr);
                 System.out.println(randomInt);
-                System.out.println("BSExist " + BSExist(arr, randomInt));
+                System.out.println("BSExist " + BSNearLeft(arr, randomInt));
                 System.out.println("Comparator " + comparator(arr, randomInt));
             }
         }
@@ -59,6 +64,14 @@ public class BSExist {
     }
 
     public static void main(String[] args) {
+//        int[] arr = Tools.generateRandomArray(10, 10);
+//        int random = Tools.getRandomInt(10);
+//        Arrays.sort(arr);
+//        Tools.printArray(arr);
+//        System.out.println(random);
+//
+//        System.out.println(BSNearLeft(arr, random));
         test();
     }
+
 }
