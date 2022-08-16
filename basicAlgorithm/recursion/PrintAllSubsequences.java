cf31a809d1a106f328ff0f23db6243cb632168c6
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2022-08-17 00:26:12 +0900

    mm

---
+++ basicAlgorithm/recursion/PrintAllSubsequences.java
@@ -0,0 +1,30 @@
+package basicAlgorithm.recursion;
+
+import java.util.ArrayList;
+
+public class PrintAllSubsequences {
+
+    public static void printAllSubsequences(String str) {
+        if (str == null || str.length() < 2) {
+            return;
+        }
+        ArrayList<String> arr = new ArrayList<>();
+        process(arr,str,"",0);
+        arr.forEach(System.out::println);
+    }
+
+    public static void process(ArrayList<String> arr, String str, String path, int index) {
+        if (index == str.length()) {
+            if (path.length() != 0) {
+                arr.add(path);
+            }
+            return;
+        }
+        process(arr, str, path, index + 1);
+        process(arr, str, path + str.charAt(index), index + 1);
+    }
+
+    public static void main(String[] args) {
+        printAllSubsequences("abcdefg");
+    }
+}
