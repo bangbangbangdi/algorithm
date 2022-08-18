8711164aae68bf4f5b287d58fb322634fa70fdd6
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2022-08-18 23:44:38 +0900

    mm

--- basicAlgorithm/dynamicProgramming/leftToRight/Knapsack.java
+++ basicAlgorithm/dynamicProgramming/leftToRight/Knapsack.java
@@ -10,11 +10,55 @@ public class Knapsack {
         if (weights == null || values == null || weights.length != values.length || weights.length < 1 || bag < 0) {
             return 0;
         }
-        return 1;
+        return process(weights, values, bag, 0);
     }
 
-    public static int process(int[] weights, int[] values, int bag ,int get,int index) {
-        return 0;
+    public static int process(int[] weights, int[] values, int rest, int index) {
+        if (rest < 0) {
+            return 0;
+        }
+        if (index == weights.length) {
+            return 0;
+        }
+
+        int res1 = process(weights, values, rest, index + 1);
+        int res2 = Integer.MIN_VALUE;
+        if (rest >= weights[index]) {
+            res2 = values[index] + process(weights, values, rest - weights[index], index + 1);
+        }
+        return Math.max(res1, res2);
+    }
+
+    public static int getMaxValue2(int[] weights, int[] values, int bag) {
+        if (weights == null || values == null || weights.length != values.length || weights.length < 1 || bag < 0) {
+            return 0;
+        }
+        return dpWays(weights, values, bag, 0);
+    }
+
+
+    public static int dpWays(int[] w, int[] v, int rest, int index) {
+        int N = w.length;
+        int[][] arr = new int[N + 1][rest+1];
+        for (int i = N - 1; i >= 0; i--) {
+            for (int j = rest; j >= 0; j--) {
+                int res1 = arr[i + 1][j];
+                int res2 = Integer.MIN_VALUE;
+                if (j >= w[i]) {
+                    res2 = v[i] + arr[i + 1][j - w[i]];
+                }
+                arr[i][j] = Math.max(res1, res2);
+            }
+        }
+        return arr[0][rest];
+    }
+
+    public static void main(String[] args) {
+        int[] weights = {3, 2, 4, 7};
+        int[] values = {5, 6, 3, 19};
+        int bag = 11;
+        System.out.println(getMaxValue(weights, values, bag));
+        System.out.println(getMaxValue2(weights, values, bag));
     }
 
 }
