0a298c46415eb43d99e958d1b58b618a1905f81e
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2022-08-18 17:22:13 +0900

    mm

--- basicAlgorithm/dynamicProgramming/leftToRight/ConvertToLetterString.java
+++ basicAlgorithm/dynamicProgramming/leftToRight/ConvertToLetterString.java
@@ -6,19 +6,33 @@ package basicAlgorithm.dynamicProgramming.leftToRight;
 // 给定一个只有数字字符组成的字符串str,返回有多少种转化结果
 public class ConvertToLetterString {
 
-    public static int number(String str){
-        if (str == null || str.length() == 0){
+    public static int number(String str) {
+        if (str == null || str.length() == 0) {
             return 0;
         }
-
-        return 1;
+        char[] chars = str.toCharArray();
+        int ans = process(chars, 0);
+        return ans;
     }
 
-    public static int process(char[] chars,int index){
-        if (index == chars.length){
+    public static int process(char[] chars, int index) {
+        if (index == chars.length) {
             return 1;
         }
-        return 2;
+        if (index > chars.length || chars[index] - 48 == 0) {
+            return 0;
+        }
+        int ans = 0;
+        if (chars[index] - 48 == 1 || (chars[index] - 48 == 2 && index + 1 < chars.length && chars[index + 1] - 48 <= 6)) {
+            ans = process(chars, index + 1) + process(chars, index + 2);
+        } else {
+            ans = process(chars, index + 1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(number("777"));
     }
 
 }
