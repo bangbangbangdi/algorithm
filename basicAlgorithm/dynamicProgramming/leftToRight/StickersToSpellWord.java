5aab03d64284152101d424760b8ca7e497d16b44
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2022-08-23 01:42:47 +0900

    mm

--- basicAlgorithm/dynamicProgramming/leftToRight/StickersToSpellWord.java
+++ basicAlgorithm/dynamicProgramming/leftToRight/StickersToSpellWord.java
@@ -1,18 +1,70 @@
 package basicAlgorithm.dynamicProgramming.leftToRight;
 
+// 给定一个字符串str 给定一个字符串类型数组 arr 只包含小写字母
+// arr里的每一个字符串,代表一张贴纸,你可以把单子字符剪开使用,目的是拼出str来
+// 每张贴纸可以用无穷多次
+// 返回至少需要多少张贴纸可以完成这个任务
+// 栗子 str = "babac" arr = {"ba","c","abcd"}
+// 至少需要两张贴纸 "ba" "abcd" 所以返回2
+
+import java.util.HashMap;
+
 public class StickersToSpellWord {
 
     public static int stickers(String str, String[] arr) {
         if (str == null || arr == null || str.length() < 1 || arr.length < 1) {
             return -1;
         }
-        char[] chars = str.toCharArray();
-//        return process(chars,0,1);
-        return 1;
+        int N = arr.length;
+        // word frequency
+        int[][] bulletWF = new int[N][26];
+        HashMap<String, Integer> dp = new HashMap<>();
+        for (int i = 0; i < N; i++) {
+            char[] chars = arr[i].toCharArray();
+            for (char cha : chars) {
+                bulletWF[i][cha - 'a']++;
+            }
+        }
+        dp.put("", 0);
+        return process(dp, str, bulletWF);
     }
 
-    public static int process(char[] chars, int count, int index, String[] arr) {
-        return 1;
+    public static int process(HashMap<String, Integer> dp, String str, int[][] bulletWF) {
+        if (dp.containsKey(str)) {
+            return dp.get(str);
+        }
+        int ans = Integer.MAX_VALUE;
+        char[] target = str.toCharArray();
+        int[] targetWF = new int[26];
+        for (char ch : target) {
+            targetWF[ch - 'a']++;
+        }
+        for (int i = 0; i < bulletWF.length; i++) {
+            if (bulletWF[i][target[0] - 'a'] == 0) {
+                continue;
+            }
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < targetWF.length; j++) {
+                if (targetWF[j] != 0) {
+                    for (int k = 0; k < Math.max(0, targetWF[j] - bulletWF[i][j]); k++) {
+                        sb.append((char) ('a' + j));
+                    }
+                }
+            }
+            String sub = sb.toString();
+            int tem = process(dp, sub, bulletWF);
+            if (tem != -1) {
+                ans = Math.min(ans, tem + 1);
+            }
+        }
+        dp.put(str,ans);
+        return dp.get(str);
+    }
+
+    public static void main(String[] args) {
+        String str = "aabbcccccddddeeee";
+        String[] arr = {"abb","ccdd","ddee"};
+        System.out.println(stickers(str, arr));
     }
 
 }
