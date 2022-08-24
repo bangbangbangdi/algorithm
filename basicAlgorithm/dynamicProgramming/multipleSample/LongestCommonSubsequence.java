8cfdaeb7f0ed0c5dd7e4e704205b5fc28beb02ad
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2022-08-24 09:19:53 +0900

    mm

--- basicAlgorithm/dynamicProgramming/multipleSample/LongestCommonSubsequence.java
+++ basicAlgorithm/dynamicProgramming/multipleSample/LongestCommonSubsequence.java
@@ -7,53 +7,50 @@ public class LongestCommonSubsequence {
         if (str1 == null || str2 == null || str1.length() < 1 || str2.length() < 1) {
             return 0;
         }
-        StringBuilder sb = new StringBuilder();
-        char[] cha1 = str1.toCharArray();
-        char[] cha2 = str2.toCharArray();
-        return process(cha1, cha2, cha1.length - 1, cha2.length - 1);
+        int N1 = str1.length();
+        int N2 = str2.length();
+        char[] chs1 = str1.toCharArray();
+        char[] chs2 = str2.toCharArray();
+        return process(chs1, chs2, N1 - 1, N2 - 1);
     }
 
-    public static int process(char[] cha1, char[] cha2, int i, int j) {
+    public static int process(char[] chs1, char[] chs2, int i, int j) {
         if (i == 0 && j == 0) {
-            return cha1[0] == cha2[0] ? 1 : 0;
+            return chs1[0] == chs2[0] ? 1 : 0;
         }
         if (i == 0) {
-            return process(cha1, cha2, i, j - 1);
+            return process(chs1, chs2, i, j - 1);
         }
         if (j == 0) {
-            return process(cha1, cha2, i - 1, j);
+            return process(chs1, chs2, i - 1, j);
         }
-        int ans = Math.max(process(cha1, cha2, i, j - 1), process(cha1, cha2, i - 1, j));
-        if (cha1[i] == cha2[j]) {
-            ans = process(cha1, cha2, i - 1, j - 1) + 1;
-        }
-        return ans;
+        return 1;
     }
 
     public static int lcs2(String str1, String str2) {
         if (str1 == null || str2 == null || str1.length() < 1 || str2.length() < 1) {
             return 0;
         }
-        char[] cha1 = str1.toCharArray();
-        char[] cha2 = str2.toCharArray();
-        return process2(cha1, cha2);
+        char[] chs1 = str1.toCharArray();
+        char[] chs2 = str2.toCharArray();
+        return process2(chs1, chs2);
     }
 
-    public static int process2(char[] cha1, char[] cha2) {
-        int N1 = cha1.length;
-        int N2 = cha2.length;
+    public static int process2(char[] chs1, char[] chs2) {
+        int N1 = chs1.length;
+        int N2 = chs2.length;
         int[][] dp = new int[N1][N2];
-        dp[0][0] = cha1[0] == cha2[0] ? 1 : 0;
+        dp[0][0] = chs1[0] == chs2[0] ? 1 : 0;
         for (int i = 1; i < N1; i++) {
-            dp[i][0] = dp[i - 1][0] == 1 ? 1 : (cha1[i] == cha2[0] ? 1 : 0);
+            dp[i][0] = dp[i - 1][0] == 1 ? 1 : (chs1[i] == chs2[0] ? 1 : 0);
         }
-        for (int i = 1; i < N2; i++) {
-            dp[0][i] = dp[0][i - 1] == 1 ? 1 : (cha2[i] == cha1[0] ? 1 : 0);
+        for (int j = 1; j < N2; j++) {
+            dp[0][j] = dp[0][j - 1] == 1 ? 1 : (chs2[j] == chs1[0] ? 1 : 0);
         }
         for (int i = 1; i < N1; i++) {
             for (int j = 1; j < N2; j++) {
-                dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
-                if (cha1[i] == cha2[j]) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                if (chs1[i] == chs2[j]) {
                     dp[i][j] = Math.max(dp[i][j], dp[i - 1][j - 1] + 1);
                 }
             }
@@ -62,9 +59,8 @@ public class LongestCommonSubsequence {
     }
 
     public static void main(String[] args) {
-        String str1 = "abcdefgqwezzxceah";
-        String str2 = "aabbdettxxaafzxcwe";
-        System.out.println(lcs(str1, str2));
+        String str1 = "abcd";
+        String str2 = "aabbeagasderqwec";
         System.out.println(lcs2(str1, str2));
     }
 }
