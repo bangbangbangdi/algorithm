8dbe07f0288638154cf3a174b045a1ddba799104
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2022-08-28 19:26:21 +0900

    mm

--- basicAlgorithm/dynamicProgramming/scope/MinPathSum.java
+++ basicAlgorithm/dynamicProgramming/scope/MinPathSum.java
@@ -9,41 +9,86 @@ public class MinPathSum {
         if (m == null || m.length < 1 || m[0] == null || m[0].length < 1) {
             return 0;
         }
-        return process(m, m.length - 1, m[0].length - 1, 0, 0, 0);
+        return process(m, m.length - 1, m[0].length - 1, 0, 0);
     }
 
-    public static int process(int[][] m, int tarX, int tarY, int curX, int curY, int count) {
+    public static int process(int[][] m, int tarX, int tarY, int curX, int curY) {
         if (curX == m.length || curY == m[0].length) {
             return Integer.MAX_VALUE;
         }
         if (curX == tarX && curY == tarY) {
-            return count + m[curX][curY];
+            return m[curX][curY];
         }
-        int p1 = process(m, tarX, tarY, curX + 1, curY, count + m[curX][curY]);
-        int p2 = process(m, tarX, tarY, curX, curY + 1, count + m[curX][curY]);
-        return Math.min(p1, p2);
+        return Math.min(process(m, tarX, tarY, curX + 1, curY), process(m, tarX, tarY, curX, curY + 1)) + m[curX][curY];
     }
 
     public static int dpWays(int[][] m) {
         if (m == null || m.length < 1 || m[0] == null || m[0].length < 1) {
             return 0;
         }
-        int max = m[0][0];
-        for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[0].length; j++) {
-                max = Math.max(m[i][j],max);
+        int[][] dp = new int[m.length][m[0].length];
+        int tarX = m.length - 1;
+        int tarY = m[0].length - 1;
+        dp[tarX][tarY] = m[tarX][tarY];
+        for (int x = tarX - 1; x >= 0; x--) {
+            dp[x][tarY] = dp[x + 1][tarY] + m[x][tarY];
+        }
+        for (int y = tarY - 1; y >= 0; y--) {
+            dp[tarX][y] = dp[tarX][y + 1] + m[tarX][y];
+        }
+        for (int x = tarX - 1; x >= 0; x--) {
+            for (int y = tarY - 1; y >= 0; y--) {
+                dp[x][y] = Math.min(dp[x + 1][y], dp[x][y + 1]) + m[x][y];
             }
         }
-        max = max * (m.length + m[0].length - 1);
-        int[][][] dp = new int[m.length][m[0].length][max];
-        return 1;
+        return dp[0][0];
+    }
+
+    public static int min2(int[][] m) {
+        if (m == null || m.length == 0 || m[0] == null || m[0].length == 0) {
+            return 0;
+        }
+        return process2(m, m.length - 1, m[0].length - 1);
+    }
+
+    public static int process2(int[][] m, int curX, int curY) {
+        if (curX == 0 && curY == 0) {
+            return m[0][0];
+        }
+        if (curX == 0) {
+            return process2(m, curX, curY - 1) + m[curX][curY];
+        }
+        if (curY == 0) {
+            return process2(m, curX - 1, curY) + m[curX][curY];
+        }
+        return Math.min(process2(m, curX - 1, curY), process2(m, curX, curY - 1)) + m[curX][curY];
+    }
+
+    public static int dpWayPro(int[][] m) {
+        if (m == null || m.length == 0 || m[0] == null || m[0].length == 0) {
+            return 0;
+        }
+        int length = m.length;
+        int width = m[0].length;
+        int[] buffer = new int[width];
+        for (int i = width - 1; i >= 0; i--) {
+            buffer[i] = i == width - 1 ? m[length - 1][i] : m[length - 1][i] + m[length - 1][i];
+        }
+        for (int x = length - 2; x >= 0; x--) {
+            for (int y = width - 1; y >= 0; y--) {
+                buffer[y] = y == width - 1 ? buffer[y] + m[x][y] : Math.min(buffer[y + 1], buffer[y]) + m[x][y];
+            }
+        }
+        return buffer[0];
     }
 
     public static void main(String[] args) {
-//        int[][] m = {{1, 3, 5, 9}, {8, 1, 3, 4}, {5, 0, 6, 1},
-//                {8, 8, 4, 0}};
-//        System.out.println(min(m));
+        int[][] m = {{1, 3, 5, 9}, {8, 1, 3, 4}, {5, 0, 6, 1},
+                {8, 8, 4, 0}};
+        System.out.println(min(m));
+        System.out.println(dpWays(m));
+        System.out.println(min2(m));
+        System.out.println(dpWayPro(m));
 
     }
-
 }
