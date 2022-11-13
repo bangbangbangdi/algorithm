db53f5510d0e13c68bc5b77908e55958d99fe244
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2022-11-14 08:39:36 +0900

    mm

--- my_practise/pro1/class02/Code01_FibonacciProblem.java
+++ my_practise/pro1/class02/Code01_FibonacciProblem.java
@@ -3,8 +3,17 @@ package my_practise.pro1.class02;
 import tool.Tools;
 
 import java.util.Arrays;
+/*
+重要结论:
+    任何递归式只要满足 : F(N) = C1*F(N-1) + C2*F(N-2) + ... + CkF(N-k)
+    则一定有logN的解
+    例如:F(N) = F(N-1) + F(N-3)  -->  C1 = 1  C2 = 0  C3 = 1  k = 3
+    F(N)*F(N-1)*F(N-2) = |F3 F2 F1| * |3*3|^n-3
+*/
+
 
 // 斐波那契额数列矩阵乘法方式实现
+
 public class Code01_FibonacciProblem {
 
     public static int f1(int N) {
@@ -80,6 +89,103 @@ public class Code01_FibonacciProblem {
         return res;
     }
 
+
+    // 以下就是一些衍生题
+    // 一个人一次可以往上迈一个台阶,也可以迈两个台阶
+    // 返回这个人迈上N级台阶的方法数
+
+    public static int s1(int N) {
+        if (N < 1) {
+            return 0;
+        }
+        if (N == 1) {
+            return 1;
+        }
+        if (N == 2) {
+            return 2;
+        }
+        return s1(N - 1) + s1(N - 2);
+    }
+
+    public static int s2(int N) {
+        if (N < 1) {
+            return 0;
+        }
+        if (N == 1) {
+            return 1;
+        }
+        if (N == 2) {
+            return 2;
+        }
+        int[] help = new int[N];
+        help[0] = 1;
+        help[1] = 2;
+        for (int i = 2; i < help.length; i++) {
+            help[i] = help[i - 1] + help[i - 2];
+        }
+        return help[N - 1];
+    }
+
+    public static int s3(int N) {
+        if (N < 1) {
+            return 0;
+        }
+        if (N == 1) {
+            return 1;
+        }
+        if (N == 2) {
+            return 2;
+        }
+        int[][] basic = {{1, 1}, {1, 0}};
+        int[][] res = matrixPower(basic, N - 2);
+        return 2 * res[0][0] + res[1][0];
+    }
+
+    // 第一年农场有一只成熟的母牛A,往后的每年
+    // 每一只成熟的母牛都会生一只母牛
+    // 每一只新出生的母牛都在出生的第三年成熟
+    // 每一只母牛永远不会死
+    // 返回N年后牛的数量(公牛做插排去了)
+
+    public static int c1(int N) {
+        if (N < 1) {
+            return 0;
+        }
+        if (N < 5) {
+            return N;
+        }
+        return c1(N - 1) + c1(N - 3);
+    }
+
+    public static int c2(int N) {
+        if (N < 1) {
+            return 0;
+        }
+        if (N < 5) {
+            return N;
+        }
+        int[] help = new int[N];
+        for (int i = 0; i < 4; i++) {
+            help[i] = i + 1;
+        }
+        for (int i = 4; i < N; i++) {
+            help[i] = help[i - 1] + help[i - 3];
+        }
+        return help[N - 1];
+    }
+
+    public static int c3(int N) {
+        if (N < 1) {
+            return 0;
+        }
+        if (N < 5) {
+            return N;
+        }
+        int[][] basic = {{1, 1, 0}, {0, 0, 1}, {1, 0, 0}};
+        int[][] res = matrixPower(basic, N - 3);
+        return 3 * res[0][0] + 2 * res[1][0] + res[2][0];
+    }
+
     public static void test() {
         int testTime = 100000;
         int maxN = 20;
@@ -90,12 +196,32 @@ public class Code01_FibonacciProblem {
             int ans1 = f1(N);
             int ans2 = f2(N);
             int ans3 = f3(N);
+            int s1 = s1(N);
+            int s2 = s2(N);
+            int s3 = s3(N);
+            int c1 = c1(N);
+            int c2 = c2(N);
+            int c3 = c3(N);
             if (ans1 != ans2 || ans1 != ans3) {
                 success = false;
                 System.out.println("N = " + N);
                 System.out.println("ans1 = " + ans1);
                 System.out.println("ans2 = " + ans2);
                 System.out.println("ans3 = " + ans3);
+            }
+            if (s1 != s2 || s1 != s3) {
+                success = false;
+                System.out.println("N = " + N);
+                System.out.println("s1 = " + s1);
+                System.out.println("s2 = " + s2);
+                System.out.println("s3 = " + s3);
+            }
+            if (c1 != c2 || c1 != c3) {
+                success = false;
+                System.out.println("N = " + N);
+                System.out.println("c1 = " + c1);
+                System.out.println("c2 = " + c2);
+                System.out.println("c3 = " + c3);
             }
         }
         System.out.println(success ? "Nice" : "F");
