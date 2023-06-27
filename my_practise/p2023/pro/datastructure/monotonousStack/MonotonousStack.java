2c67788cf1f3420db38a222697aadccccbc7b438
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2023-06-27 23:08:10 +0900

    mm

--- my_practise/p2023/pro/datastructure/monotonousStack/MonotonousStack.java
+++ my_practise/p2023/pro/datastructure/monotonousStack/MonotonousStack.java
@@ -1,7 +1,24 @@
 package my_practise.p2023.pro.datastructure.monotonousStack;
 
+/*
+ * @param null:
+  * @return null
+ * @author BangDi
+ * @description
+ * 单调栈是什么?
+ * 一种特别设计的栈结构，为了解决如下的问题：
+ * 给定一个可能含有重复值的数组arr，i位置的数一定存在如下两个信息
+ * 1）arr[i]的左侧离i最近并且小于(或者大于)arr[i]的数在哪？
+ * 2）arr[i]的右侧离i最近并且小于(或者大于)arr[i]的数在哪？
+ * 如果想得到arr中所有位置的两个信息，怎么能让得到信息的过程尽量快。
+ *
+ *给定一个只包含正数的数组arr，arr中任何一个子数组sub，
+ * 一定都可以算出(sub累加和 )* (sub中的最小值)是什么，
+ * 那么所有子数组中，这个值最大是多少？
+ *
+ * @date 2023/6/27 23:06
+ */
 public class MonotonousStack {
-
 
 
 }
