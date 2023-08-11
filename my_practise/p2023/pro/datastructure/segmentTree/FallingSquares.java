1f5a8fd054be2c2b0415702cb015a73f89b2a63c
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2023-08-11 11:09:01 +0900

    mm

--- my_practise/p2023/pro/datastructure/segmentTree/FallingSquares.java
+++ my_practise/p2023/pro/datastructure/segmentTree/FallingSquares.java
@@ -5,7 +5,16 @@ package my_practise.p2023.pro.datastructure.segmentTree;
  * package:        my_practise.p2023.pro.datastructure.segmentTree
  * className:      FallingSquares
  * author:     BangDi
- * description:  TODO
+ * description:
+ 想象一下标准的俄罗斯方块游戏，X轴是积木最终下落到底的轴线
+ 下面是这个游戏的简化版：
+ 1）只会下落正方形积木
+ 2）[a,b] -> 代表一个边长为b的正方形积木，积木左边缘沿着X = a这条线从上方掉落
+ 3）认为整个X轴都可能接住积木，也就是说简化版游戏是没有整体的左右边界的
+ 4）没有整体的左右边界，所以简化版游戏不会消除积木，因为不会有哪一层被填满。
+
+ 给定一个N*2的二维数组matrix，可以代表N个积木依次掉落，
+ 返回每一次掉落之后的最大高度
  * date:    2023/8/11 11:01
  * version:    1.0
  */
