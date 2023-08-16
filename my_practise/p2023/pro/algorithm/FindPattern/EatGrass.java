256d2e7922679b41b6faf00039bb52f1633a7cde
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2023-08-16 23:33:52 +0900

    mm

---
+++ my_practise/p2023/pro/algorithm/FindPattern/EatGrass.java
@@ -0,0 +1,13 @@
+package my_practise.p2023.pro.algorithm.FindPattern;
+
+/**
+ * projectName:    algorithm
+ * package:        my_practise.p2023.pro.algorithm.FindPattern
+ * className:      EatGrass
+ * author:     BangDi
+ * description:  TODO
+ * date:    2023/8/16 23:33
+ * version:    1.0
+ */
+public class EatGrass {
+}
