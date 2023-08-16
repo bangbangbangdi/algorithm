58acf1fb412e9800384f051416acc77d542014af
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2023-08-16 22:24:57 +0900

    mm

---
+++ my_practise/p2023/pro/algorithm/FindPattern/AppleMinBags.java
@@ -0,0 +1,13 @@
+package my_practise.p2023.pro.algorithm.FindPattern;
+
+/**
+ * projectName:    algorithm
+ * package:        my_practise.p2023.pro.algorithm.FindPattern
+ * className:      AppleMinBags
+ * author:     BangDi
+ * description:  TODO
+ * date:    2023/8/16 22:24
+ * version:    1.0
+ */
+public class AppleMinBags {
+}
