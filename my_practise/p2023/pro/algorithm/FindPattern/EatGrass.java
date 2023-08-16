6bcfb117ee4cc63da6704ac46aaabd67882f1515
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2023-08-16 23:34:43 +0900

    mm

--- my_practise/p2023/pro/algorithm/FindPattern/EatGrass.java
+++ my_practise/p2023/pro/algorithm/FindPattern/EatGrass.java
@@ -5,7 +5,14 @@ package my_practise.p2023.pro.algorithm.FindPattern;
  * package:        my_practise.p2023.pro.algorithm.FindPattern
  * className:      EatGrass
  * author:     BangDi
- * description:  TODO
+ * description:
+ * 给定一个正整数N，表示有N份青草统一堆放在仓库里
+ * 有一只牛和一只羊，牛先吃，羊后吃，它俩轮流吃草
+ * 不管是牛还是羊，每一轮能吃的草量必须是：
+ * 1，4，16，64…(4的某次方)
+ * 谁最先把草吃完，谁获胜
+ * 假设牛和羊都绝顶聪明，都想赢，都会做出理性的决定
+ * 根据唯一的参数N，返回谁会赢
  * date:    2023/8/16 23:33
  * version:    1.0
  */
